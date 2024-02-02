d81d2eeb2835eb61313285ce30999c3bcd10a27a
Lavken <lavkenpilar@gmail.com>
2024-02-02 01:36:15 -0300

    create index-js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const cuotas = 10
+if (cuotas >= 6) {
+    console.log ("Serán aplicados intereses en esta compra")
+    } else {
+        console.log ("No serán aplicados intereses en esta compra")
+    }
